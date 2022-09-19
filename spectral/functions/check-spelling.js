7e57ce843c35ce42391f824d5c7fdeed98926ee1
Alex Akimov <alex.akimov@monite.com>
2022-09-19 21:07:32 +0200

    Added check for code apostrophes

--- spectral/functions/check-spelling.js
+++ spectral/functions/check-spelling.js
@@ -1,11 +1,14 @@
 const spellChecker = require('spellchecker');
-const exceptions = ["Jinja2","bic","iban"];
+const exceptions = ["Jinja2","asc","bic","iban"];
 
 const separatorsRegex = /\s/     // any whitespace
 
 export default (input) => {
 
   const words = input.split(separatorsRegex);
+  words.forEach(function(part, index, theArray) {
+    theArray[index] = part.replace(/`/g, '');
+  });
   const mistakes = words
     .filter((word) => !exceptions.includes(word))
     .filter((word) => spellChecker.isMisspelled(word));
